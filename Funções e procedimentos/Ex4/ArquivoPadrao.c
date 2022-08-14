8ddf3195c18220007723ba953e5e90139b381d87
maiconrdgz <95759562+maiconrdgz@users.noreply.github.com>
2022-08-14 17:57:51 -0300

    Create ArquivoPadrao.c

---
+++ Funções e procedimentos/Ex4/ArquivoPadrao.c
@@ -0,0 +1,41 @@
+/* ===== Inclusão de bibliotecas ====== */
+
+#include <stdlib.h>
+#include <stdio.h>  
+#include <math.h>   // Função para Fórmulas matemáticas
+#include <string.h> // Função que manipula strings
+
+
+
+/* ===== Protótipo das funções ====== */
+
+
+
+
+
+
+/* ===== Variáveis Globais ====== */
+
+
+
+
+
+
+/* ===== Função Principal ====== */
+
+int main()
+{
+    
+
+
+
+
+    system("PAUSE");
+    return 0;
+}
+
+
+/* ===== Desenvolvimento das Funções ====== */
+
+
+
